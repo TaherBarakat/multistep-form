23b15d491644def359d67d0251dfb28c8b620ac7
Taher Barakat <taher.barakat12223@gmail.com>
2024-04-04 20:53:48 +0300

    add other component that represent the remaining form steps

---
+++ src/AccountForm.tsx
@@ -0,0 +1,10 @@
+export default function AccountForm() {
+     return (
+          <>
+               <label>Email</label>
+               <input required autoFocus type="email" />
+               <label>Password</label>
+               <input required type="password" />
+          </>
+     );
+}
